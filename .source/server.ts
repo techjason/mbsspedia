ff62c675e939ea80dc183b1b18c61a24b9af1d7a
techjason <thetechjason@gmail.com>
2026-02-08 17:30:22 +0800

    Update SKIP_DESCEND and SKIP_REWRITE_IN_PARENT sets to include 'heading'
    - Added 'heading' to both SKIP_DESCEND and SKIP_REWRITE_IN_PARENT sets in source.config.mjs and remark-citations.ts.
    - This change ensures that heading elements are properly handled during text processing and citation splitting.

--- .source/server.ts
+++ .source/server.ts
@@ -1,11 +1,16 @@
 // @ts-nocheck
-import * as __fd_glob_9 from "../content/docs/medicine/placeholder.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/medicine/index.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/general-surgery/liver-cirrhosis.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/general-surgery/liver-abscess.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/general-surgery/jaundice.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/general-surgery/index.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/general-surgery/acute-pancreatitis.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/medicine/placeholder.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/medicine/index.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/general-surgery/pancreatic-cancer.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/general-surgery/mirizzi-syndrome.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/general-surgery/liver-cirrhosis.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/general-surgery/liver-abscess.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/general-surgery/jaundice.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/general-surgery/index.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/general-surgery/gallbladder-cancer.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/general-surgery/cholangiocarcinoma.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/general-surgery/acute-pancreatitis.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/general-surgery/acute-cholecystitis.mdx?collection=docs"
 import * as __fd_glob_2 from "../content/docs/index.mdx?collection=docs"
 import { default as __fd_glob_1 } from "../content/docs/medicine/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/general-surgery/meta.json?collection=docs"
@@ -17,4 +22,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"general-surgery/meta.json": __fd_glob_0, "medicine/meta.json": __fd_glob_1, }, {"index.mdx": __fd_glob_2, "general-surgery/acute-pancreatitis.mdx": __fd_glob_3, "general-surgery/index.mdx": __fd_glob_4, "general-surgery/jaundice.mdx": __fd_glob_5, "general-surgery/liver-abscess.mdx": __fd_glob_6, "general-surgery/liver-cirrhosis.mdx": __fd_glob_7, "medicine/index.mdx": __fd_glob_8, "medicine/placeholder.mdx": __fd_glob_9, });
+export const docs = await create.docs("docs", "content/docs", {"general-surgery/meta.json": __fd_glob_0, "medicine/meta.json": __fd_glob_1, }, {"index.mdx": __fd_glob_2, "general-surgery/acute-cholecystitis.mdx": __fd_glob_3, "general-surgery/acute-pancreatitis.mdx": __fd_glob_4, "general-surgery/cholangiocarcinoma.mdx": __fd_glob_5, "general-surgery/gallbladder-cancer.mdx": __fd_glob_6, "general-surgery/index.mdx": __fd_glob_7, "general-surgery/jaundice.mdx": __fd_glob_8, "general-surgery/liver-abscess.mdx": __fd_glob_9, "general-surgery/liver-cirrhosis.mdx": __fd_glob_10, "general-surgery/mirizzi-syndrome.mdx": __fd_glob_11, "general-surgery/pancreatic-cancer.mdx": __fd_glob_12, "medicine/index.mdx": __fd_glob_13, "medicine/placeholder.mdx": __fd_glob_14, });
